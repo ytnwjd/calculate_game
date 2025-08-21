dce682bf073f8f89170b781c47f3310fd859f7d1
y.tnwjd <ysjsw@ajou.ac.kr>
2025-08-21 19:55:00 +0900

    change font size

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1> 맞 춰 봐 </h1>
+        <h1> 4교시 수학시간 </h1>
         <p>30초 안에 정답을 맞추지 못하면... </p>
       </header>
 
