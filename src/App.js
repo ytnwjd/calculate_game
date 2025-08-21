fd4a29c6488d8828e354d538017d7c471f2c686e
y.tnwjd <ysjsw@ajou.ac.kr>
2025-08-21 21:39:48 +0900

    change time

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <h1> 4교시 수학시간 </h1>
-        <p>30초 안에 정답을 맞추지 못하면... </p>
+        <p>10초 안에 정답을 맞추지 못하면... </p>
       </header>
 
       <main>
