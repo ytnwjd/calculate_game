024b02bd13ecb713efebadf1d5ecb5b40285c610
y.tnwjd <ysjsw@ajou.ac.kr>
2025-08-21 19:14:27 +0900

    App style

--- src/App.js
+++ src/App.js
@@ -5,10 +5,10 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1> </h1>
+        <h1> 맞 춰 봐 </h1>
         <p>30초 안에 정답을 맞추지 못하면... </p>
       </header>
-      
+
       <main>
         <MathGame />
       </main>
